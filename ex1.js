bd18afc7ffc67551c367c4c8560d3089ccf40b15
vggochi <valentina.neves@aluno.senai.br>
2026-03-24 12:12:20 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,10 @@
+// Contexto: Seiya possui um cosmoBase de 100. Ao usar o Meteoro de Pégaso, seu cosmo é multiplicado por 5, e em seguida, ele sofre uma penalidade exaustiva de 50 pontos.
+// Missão: Escreva as operações em JavaScript usando operadores de atribuição composta. Qual é o valor final da variável cosmoBase?
+let cosmoBase = 100;
+
+// operações:
+cosmoBase *= 5; //multiplica por 5
+cosmoBase -= 50; //subtrair por 50
+
+console.log("CosmoBase Final:", cosmoBase);
+//450
---
+++ ex2.js
@@ -0,0 +1,9 @@
+let armadura = false;
+let cosmoShiryu = 8000;
+let cosmoInimigo = 7500;
+
+//expressão
+let vitoria = !armadura && cosmoShiryu > cosmoInimigo;
+
+console.log("Vitória: ", vitoria);
+// true
